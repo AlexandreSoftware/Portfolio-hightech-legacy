a2379d78884ea7de2d0d3e2f562811e9c2b9a6de
xandrf <oliveira_carlos2017@Outlook.com.br>
2021-08-16 17:19:43 -0300

    added draft of portfolio data, adding footer and fixing navbar next so that i can do a first release

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import "./css/App.css"
+import CarrerSummary from "./CarrerSummary"
 function App() {
   return (
     <div id="app">
       <h1 class="titulo">Carlos Alexandre De oliveira Junior</h1>
+      <CarrerSummary />
     </div>
   );
 }
---
+++ app/src/CarrerSummary.js
@@ -0,0 +1,12 @@
+
+function CarrerSummary(){
+    return (
+        <p>
+            Hi, I'm Carlos (also known as Xandrf),
+            a fullstack software engineer from Sao Paulo/SP:Brazil
+            
+
+        </p>
+    )
+}
+export default CarrerSummary;
